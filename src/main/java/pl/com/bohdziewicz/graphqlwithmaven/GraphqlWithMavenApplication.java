80ae1337dafd16500e50cc64b49f2d8b9c8f9a2f
AsystentRoberta <asystent.roberta@gmail.com>
2020-03-06 16:11:35 +0100

    First files created with maven

---
+++ src/main/java/pl/com/bohdziewicz/graphqlwithmaven/GraphqlWithMavenApplication.java
@@ -0,0 +1,13 @@
+package pl.com.bohdziewicz.graphqlwithmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlWithMavenApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GraphqlWithMavenApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/com/bohdziewicz/graphqlwithmaven/GraphqlWithMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.bohdziewicz.graphqlwithmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlWithMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
