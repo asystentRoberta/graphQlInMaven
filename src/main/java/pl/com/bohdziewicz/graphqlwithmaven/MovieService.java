b3b20743de9b939f9175fa628b8c713b54f20dc3
AsystentRoberta <asystent.roberta@gmail.com>
2020-03-06 17:49:32 +0100

    Set @Service annotation (MovieQuery require movieService bean - look at previous commit)

--- src/main/java/pl/com/bohdziewicz/graphqlwithmaven/MovieService.java
+++ src/main/java/pl/com/bohdziewicz/graphqlwithmaven/MovieService.java
@@ -5,10 +5,12 @@ import java.util.stream.Collectors;
 
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
 
 import pl.com.bohdziewicz.graphqlwithmaven.dao.Movie;
 import pl.com.bohdziewicz.graphqlwithmaven.dao.MovieRepo;
 
+@Service
 public class MovieService {
 
     private MovieRepo movieRepo;
