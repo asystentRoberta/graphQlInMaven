1871c4178b30866a43a330a0052f7696b8422223
AsystentRoberta <asystent.roberta@gmail.com>
2020-03-10 16:18:25 +0100

    Try start postgres database on heroku servers

--- src/main/java/pl/com/bohdziewicz/graphqlwithmaven/dao/Movie.java
+++ src/main/java/pl/com/bohdziewicz/graphqlwithmaven/dao/Movie.java
@@ -5,12 +5,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Setter @Getter @NoArgsConstructor
+@Setter @Getter @NoArgsConstructor @AllArgsConstructor
 public class Movie {
 
     @Id
